17b10e5de6eacf19e17fff8ac3c1b2b623f5c60d
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-25 22:00:35 +0100

    Napravljen interfejs Radnja i abstraktna klasa VulkanizerskaRadnja koja implementira metode inerfejsa Radnja

---
+++ src/auto_radnja/Radnja.java
@@ -0,0 +1,13 @@
+package auto_radnja;
+
+import java.util.LinkedList;
+
+import auto_radnja.gume.AutoGuma;
+
+public interface Radnja {
+	
+	void dodajGumu(AutoGuma a);
+	
+	LinkedList<AutoGuma> pronadjiGumu(String markaModel);
+
+}
